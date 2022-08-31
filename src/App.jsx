b19314e4a499734ce78b9bd3b7541568de722aa6
SosaMati <matiasezequielsosa@gmail.com>
2022-08-31 02:49:01 -0300

    add Navigate to /clientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 import { EditarCliente } from './paginas/EditarCliente';
 import { Inicio } from './paginas/Inicio';
@@ -11,15 +10,21 @@ import { VerCliente } from './paginas/VerCliente';
 
 function App() {
 
+
+  
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path='/clientes' element={ <Layout/> }>
           <Route index element={ <Inicio /> }/>
           <Route path='nuevo' element={ <NuevoCliente /> }/>
           <Route path='editar/:id' element={ <EditarCliente /> }/>
           <Route path=':id' element={ <VerCliente /> }/>
         </Route>
+
+          <Route path="/*" element={ <Navigate to= '/clientes'/> } /> 
+          
       </Routes>
     </BrowserRouter>
   )
