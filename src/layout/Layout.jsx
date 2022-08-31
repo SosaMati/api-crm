0563094a67695b9a483e770b0f872743622295a4
SosaMati <matiasezequielsosa@gmail.com>
2022-08-31 15:48:08 -0300

    change title

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -10,7 +10,7 @@ export const Layout = () => {
     <div className='md:flex md:min-h-screen bg-gray-50'>
 
         <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
-            <h2 className='text-4xl font-black text-center text-white'>CMR - Clientes</h2>
+            <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
             <nav className='mt-10'>
 
                 <Link 
